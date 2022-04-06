b414ef643c93be792863b17c9a627df79ec9d5cf
asqarDev <asqarmirzayev2@gamil.com>
2022-04-06 23:21:47 +0500

    myreactapp qo'shildi

---
+++ myzakoreact/src/Componenets/HomePage.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Sarlavha h1</h1>
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
+
+
