9bd356e0b0faf3a9546131090b2fe533bbd82e07
asqarDev <asqarmirzayev2@gamil.com>
2022-04-07 18:26:15 +0500

    commit

--- myzakoreact/src/App.js
+++ myzakoreact/src/App.js
@@ -3,8 +3,8 @@ import HomePage from "./Componenets/HomePage";
 function App() {
   return (
     <div className="">
-      <p>Hello react</p>
-      <h1>salom react</h1>
+      {/* <p>Hello react</p>
+      <h1>salom react</h1> */}
       <HomePage />
     </div>
   );
--- myzakoreact/src/Componenets/HomePage.js
+++ myzakoreact/src/Componenets/HomePage.js
@@ -1,19 +1,48 @@
 import React, { Component } from "react";
-
+import "./HomePage.css";
 export default class HomePage extends Component {
+  state = {
+    count: 0,
+
+    
+  };
+
+  handleOk = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  handleMuns = () => {
+    if (this.state.count > 0) {
+      this.setState({
+        count: this.state.count - 1,
+      });
+    }
+  };
+
+  handleReset = () => {
+    this.setState({
+      count: 0,
+    });
+  };
+
   render() {
     return (
       <div className="container">
-        <h1>Sarlavha h1</h1>
+        <h1 className="h1">Funksiyalar</h1>
+
+        <div className="row">
+          <div className="col">
+            <div className="card">
+              <h3>Count: {this.state.count}</h3>
+              <button onClick={this.handleMuns}>count -</button>
+              <button onClick={this.handleReset}>Reset</button>
+              <button onClick={this.handleOk}>count +</button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
-
-
-
-
-
-
-
-
