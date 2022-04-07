f5c4a76b319af7862b3de80191a67ba1d0504a12
asqarDev <asqarmirzayev2@gamil.com>
2022-04-07 20:52:15 +0500

    styles

--- myzakoreact/src/Componenets/HomePage.js
+++ myzakoreact/src/Componenets/HomePage.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import "./HomePage.css";
+import globs from "./../img/pexels-pixabay-302743.jpg";
+import { Button, Card, Container, Row } from "react-bootstrap";
 export default class HomePage extends Component {
   state = {
     count: 0,
-
-    
   };
 
   handleOk = () => {
@@ -41,6 +41,22 @@ export default class HomePage extends Component {
               <button onClick={this.handleOk}>count +</button>
             </div>
           </div>
+
+          <Container>
+            <Row>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={globs} />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Row>
+          </Container>
         </div>
       </div>
     );
